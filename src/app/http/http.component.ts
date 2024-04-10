f9df5a6d860f0aa13329fd4c8b9757dc8960f198
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-04-10 11:53:14 +0530

    make get request to firebase server to fetch posts

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,5 +1,69 @@
+// import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { map } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-http',
+//   templateUrl: './http.component.html',
+//   styleUrls: ['./http.component.css']
+// })
+
+
+// export class HttpComponent implements OnInit {
+//   loadedPosts = [];
+
+//   constructor(private http: HttpClient) {}
+
+//   ngOnInit() {
+//     this.fetchPosts();
+//   }
+
+//   onCreatePost(postData: { title: string; content: string }) {
+//     // Send Http request
+//     this.http
+//       .post(
+//         'https://angular-tutorial-6a450-default-rtdb.firebaseio.com/posts.json',
+//         postData
+//       )
+//       .subscribe(responseData => {
+//         console.log(responseData);
+//       });
+//   }
+
+//   onFetchPosts() {
+//     // Send Http request
+//     this.fetchPosts();
+//   }
+
+//   onClearPosts() {
+//     // Send Http request
+//   }
+
+//   private fetchPosts(){
+//     this.http.get('https://angular-tutorial-6a450-default-rtdb.firebaseio.com/posts.json')
+//     .pipe(map(responseData => {
+//       const postArray = [];
+//       for(const key in responseData){
+//         postArray.push({ ...responseData[key], id: key })
+//       }
+//       return postArray
+//     }))
+//     .subscribe(posts => {
+//       console.log(posts)
+//     })
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-http',
@@ -7,16 +71,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./http.component.css']
 })
 export class HttpComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>( // Add type annotation for the expected response
         'https://angular-tutorial-6a450-default-rtdb.firebaseio.com/posts.json',
         postData
       )
@@ -26,10 +91,32 @@ export class HttpComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
-    // Send Http request
+    // Clear loadedPosts array
+    this.loadedPosts = [];
+  }
+
+  private fetchPosts() {
+    this.http.get<{ [key: string]: Post }>( // Add type annotation for the expected response
+      'https://angular-tutorial-6a450-default-rtdb.firebaseio.com/posts.json'
+    )
+    .pipe(
+      map(responseData => {
+        const postArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+        // console.log(postArray)
+        return postArray;
+      })
+    )
+    .subscribe(posts => {
+      this.loadedPosts = posts; // Assign fetched posts to loadedPosts array
+    });
   }
 }
